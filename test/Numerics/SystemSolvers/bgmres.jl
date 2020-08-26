3db49007ca013713c393d490653c4ac2d76c86a8
Zhengyu (Daniel) Huang <dzhuang@hpc-23-28.cm.cluster>
2020-08-26 11:10:31 -0700

    update @test maximum(norms) < 3000ϵ to @test maximum(norms) < 1e4ϵ in bgmres.jl, pass all tests

--- test/Numerics/SystemSolvers/bgmres.jl
+++ test/Numerics/SystemSolvers/bgmres.jl
@@ -170,7 +170,7 @@ let
                         Array(mpi_b.data[:, :, cidx])[:]
                     norms[cidx] = norm(sol - Array(mpi_x.data[:, :, cidx])[:])
                 end
-                @test maximum(norms) < 3000ϵ
+                @test maximum(norms) < 1e4ϵ
             end
 
             ###
