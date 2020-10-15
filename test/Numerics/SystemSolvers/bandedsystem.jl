5a2988c83c0365e24d98d2b2d5ea933e70d4da3d
Jeremy E Kozdon <jekozdon@nps.edu>
2020-10-15 09:36:49 -0700

    Update files to use LocalGeometry

--- test/Numerics/SystemSolvers/bandedsystem.jl
+++ test/Numerics/SystemSolvers/bandedsystem.jl
@@ -50,10 +50,10 @@ function initial_condition!(
     ::Pseudo1D{n, α, β, μ, δ},
     state,
     aux,
-    x,
+    coord,
     t,
 ) where {n, α, β, μ, δ}
-    ξn = dot(n, x)
+    ξn = dot(n, coord)
     # ξT = SVector(x) - ξn * n
     state.ρ = exp(-(ξn - μ - α * t)^2 / (4 * β * (δ + t))) / sqrt(1 + t / δ)
 end
