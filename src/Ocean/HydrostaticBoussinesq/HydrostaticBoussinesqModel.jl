fe59807ebb83d882934a4b497b7d6cb14b92d5e2
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2020-10-26 11:59:54 -0700

    Really fix basic_grid_info

--- src/Ocean/HydrostaticBoussinesq/HydrostaticBoussinesqModel.jl
+++ src/Ocean/HydrostaticBoussinesq/HydrostaticBoussinesqModel.jl
@@ -710,13 +710,14 @@ function update_auxiliary_state_gradient!(
     number_aux = number_states(m, Auxiliary())
     index_wz0 = varsindex(vars_state(m, Auxiliary(), FT), :wz0)
     info = basic_grid_info(dg)
-    Nqh, Nqk = info.Nq^2, info.Nqk
-    nvertelem, nhorzelem = info.nvertelem, info.nhorzelem
+    Nq, Nqk = info.Nq, info.Nqk
+    nelemv, nelemh = info.nvertelem, info.nhorzelem
+    nrealelemh = info.nhorzrealelem
 
     # project w(z=0) down the stack
-    data = reshape(A.data, Nqh, Nqk, number_aux, nvertelem, nhorzelem)
-    flat_wz0 = @view data[:, end:end, index_w, end:end, 1:(info.nhorzrealelem)]
-    boxy_wz0 = @view data[:, :, index_wz0, :, 1:(info.nhorzrealelem)]
+    data = reshape(A.data, Nq^2, Nqk, number_aux, nelemv, nelemh)
+    flat_wz0 = @view data[:, end:end, index_w, end:end, 1:nrealelemh]
+    boxy_wz0 = @view data[:, :, index_wz0, :, 1:nrealelemh]
     boxy_wz0 .= flat_wz0
 
     return true
